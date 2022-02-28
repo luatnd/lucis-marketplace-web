f8ba2e1048a46fe18235c6e320af5d6d757f4039
ductnn <trannhuduc531998@gmail.com>
2022-02-28 21:22:56 +0700

    Fix ESlint

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import type { AppProps } from "next/app"
 import React, { useEffect, useState } from "react"
 import { AppLayout } from "../components/AppLayout"
