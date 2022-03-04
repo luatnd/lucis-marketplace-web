411beb8d7bbe8d6773259fb08f059a999e21e4a9
Trần Duy Trung <trungtd@animverse.com>
2022-03-04 10:41:09 +0700

    style: improve home page style

--- src/components/Home/Banners.tsx
+++ src/components/Home/Banners.tsx
@@ -1,52 +1,9 @@
 import { Button, Icon } from "@chakra-ui/react"
 import { ChevronLeft, ChevronRight } from "react-feather"
 import Slider from "react-slick"
-
-const PrevArrow = (props) => {
-  const { onClick } = props
-  return (
-    <Button className="arrow-button arrow-prev">
-      <Icon as={ChevronLeft} onClick={onClick} />
-    </Button>
-  )
-}
-const NextArrow = (props) => {
-  const { onClick } = props
-  return (
-    <Button className="arrow-button arrow-next">
-      <Icon as={ChevronRight} onClick={onClick} />
-    </Button>
-  )
-}
+import Marquee from "react-fast-marquee"
 
 export const Banners = () => {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    swipeToSlide: true,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      {
-        breakpoint: 855,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-    ],
-  }
-
   const items = [
     {
       key: "1",
@@ -64,21 +21,22 @@ export const Banners = () => {
       key: "4",
       image: "/home/banners/banner4.png",
     },
-    {
-      key: "5",
-      image: "/home/banners/banner4.png",
-    },
   ]
 
   return (
     <div className="banners">
-      <Slider {...settings}>
-        {items.map((item) => (
-          <div key={item.key} className="banner">
-            <img src={item.image} className="banner-image" />
+      <Marquee speed={100} gradient={false}>
+        {items.map((item, index) => (
+          <div
+            key={item.key}
+            className={`banner-border ${index % 2 !== 0 ? "even-banner" : ""}`}
+          >
+            <div key={item.key} className="banner">
+              <img src={item.image} className="banner-image" />
+            </div>
           </div>
         ))}
-      </Slider>
+      </Marquee>
     </div>
   )
 }
